d8d6fb029cbf9c27091ba1577d59d6bc75d65e1c
inzahanyang <inzahanyang@gmail.com>
2021-02-27 20:02:27 +0900

    Updates

--- expending-cards/script.js
+++
@@ -1,9 +0,0 @@
-const panels = document.querySelectorAll('.panel');
-
-panels.forEach((panel) => {
-    panel.addEventListener('click', (e) => {
-        panels.forEach((panel) => panel.classList.remove('active'));
-
-        panel.classList.add('active');
-    });
-});
