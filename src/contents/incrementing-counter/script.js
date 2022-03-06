dc2ea7088d4fd9c70b67292ee9724ca894f51a08
inzahanyang <inzahanyang@gmail.com>
2022-03-06 17:02:27 +0900

    incrementing-counter

--- src/contents/incrementing-counter/script.js
+++ src/contents/incrementing-counter/script.js
@@ -3,14 +3,14 @@ const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   counter.innerText = `0`;
 
-  const updateCounter = (a) => {
+  const updateCounter = () => {
     const target = +counter.getAttribute("data-target");
     const c = +counter.innerHTML;
 
     const increment = target / 200;
 
     if (c < target) {
-      console.log((counter.innerText = `${Math.ceil(c + increment)}`));
+      counter.innerText = `${Math.ceil(c + increment)}`;
       setTimeout(updateCounter, 1);
     } else {
       counter.innerText = target;
