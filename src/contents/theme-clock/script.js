09ed7f23425e85c8f54c28f5fdf40d8783afaaee
inzahanyang <inzahanyang@gmail.com>
2022-04-01 21:39:30 +0900

    theme-clock

---
+++ src/contents/theme-clock/script.js
@@ -0,0 +1,79 @@
+const toggleEl = document.querySelector(".toggle");
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+toggleEl.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hourForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const amPm = hours >= 12 ? "PM" : "AM";
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${
+    hourForClock * (360 / 12)
+  }deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${
+    minutes * (360 / 60)
+  }deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${
+    seconds * (360 / 60)
+  }deg)`;
+
+  timeEl.innerHTML = `${hourForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${amPm}`;
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span> `;
+
+  // hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`;
+  // minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes,0,59,0,360)}deg)`;
+  // secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds,0,59,0,360)}deg)`;
+}
+
+// const scale = (num, in_min, in_max, out_min, out_max) =>  ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+
+setTime();
+setInterval(setTime, 1000);
