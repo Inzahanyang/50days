bc2c1ae4fa29651f127a3fea18f42a50cd606339
inzahanyang <inzahanyang@gmail.com>
2022-05-31 08:46:04 +0900

    pokedex

---
+++ src/contents/pokedex/script.js
@@ -0,0 +1,72 @@
+const pokemonContainer = document.getElementById("pokemon-container");
+
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const mainTypes = Object.keys(colors);
+
+const pokemonCount = 150;
+
+const fetchData = async () => {
+  for (let i = 1; i <= pokemonCount; i++) {
+    await getData(i);
+  }
+};
+
+const getData = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  addToDOM(data);
+};
+
+const addToDOM = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const id = pokemon.id.toString().padStart(3, "0");
+  const pokeTypes = pokemon.types.map((type) => type.type.name);
+  const types = mainTypes.find((type) => pokeTypes.indexOf(type) > -1);
+  const typeName = types[0].toUpperCase() + types.slice(1);
+  const color = colors[types];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+  
+    <div class="img-container">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"
+        alt="pokemon"
+      />
+    </div>
+  
+    <div class="info">
+      <span class="number"><small>#</small>${id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${typeName}</span></small>
+    </div>
+  `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  pokemonContainer.appendChild(pokemonEl);
+};
+
+fetchData();
