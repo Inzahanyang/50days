4745c6202f0d2398a3b088a0c7d0484dcef9e48c
inzahanyang <inzahanyang@gmail.com>
2022-07-10 00:55:17 +0900

    Updates

---
+++ contents/todo-list/script.js
@@ -0,0 +1,64 @@
+const form = document.getElementById("form");
+const todosUL = document.getElementById("todos");
+const input = document.getElementById("input");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+const updateLs = () => {
+  const todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todo) => {
+    todos.push({
+      text: todo.childNodes[0].data,
+      completed: todo.classList.contains("completed"),
+      date: todo.lastChild.innerText,
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const addTodo = (todo) => {
+  let todoText = input.value;
+  let todoDate = new Date().toLocaleString();
+
+  if (todo) {
+    todoText = todo.text;
+    todoDate = todo.date;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLs();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+      updateLs();
+    });
+
+    todoEl.innerHTML = `${todoText} <br> <span>${todoDate}</span>`;
+
+    todosUL.appendChild(todoEl);
+    input.value = "";
+
+    updateLs();
+  }
+};
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
